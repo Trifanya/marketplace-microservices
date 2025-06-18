882c87d0b697ac48e5df938e2dba2b4cf219cffc
Nikita Trifonov <trifanya.dev@gmail.com>
2025-06-18 11:56:55 +0300

    Удаление security-кода из account-service

--- account-service/impl/src/main/java/dev/trifonov/account_service/config/FeignConfig.java
+++ account-service/impl/src/main/java/dev/trifonov/account_service/config/FeignConfig.java
@@ -11,39 +11,8 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
-/**
- * @ConditionalOnBean - бин будет создан, только если в контексте приложения есть бины указанных типов.
- * @ConditionalOnMissingBean - бин будет создан, если в контексте приложения нет бинов указанных типов.
- * @ConditionalOnProperty - бин будет создан, только если указанное свойство присутствует и имеет значение.
- */
-
-//@Configuration
 @RequiredArgsConstructor
 public class FeignConfig {
-
-    //private final OAuth2ClientProperties oAuth2ClientProperties;
-
-    @Bean
-    @ConditionalOnBean({OAuth2AuthorizedClientService.class, ClientRegistrationRepository.class})
-    public OAuth2AuthorizedClientManager feignOAuth2AuthorizedClientManager(
-            ClientRegistrationRepository clientRegistrationRepository,
-            OAuth2AuthorizedClientService oAuth2AuthorizedClientService) {
-        return new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository,
-                oAuth2AuthorizedClientService);
-    }
-
-    @Bean
-    @ConditionalOnBean(OAuth2AuthorizedClientManager.class)
-    public OAuth2AccessTokenInterceptor defaultOAuth2AccessTokenInterceptor(
-            OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager) {
-        return new OAuth2AccessTokenInterceptor("keycloak", oAuth2AuthorizedClientManager);
-    }
-
-/*    @Bean
-    public RequestInterceptor requestInterceptor(@Value("${feign-client.catalog-service.username}") String username,
-                                                 @Value("${feign-client.catalog-service.password}") String password) {
-        return new BasicAuthRequestInterceptor(username, password);
-    }*/
 
     @Bean
     public Logger.Level feignLoggerLevel() {
